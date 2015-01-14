c58143bd2991de6e2905eb30a3a76033b556f454
Wenjun Che <wenjun@openf.in>
2015-01-14 09:05:13 -0500

    RUN-725: Fixed minor issue with passing RVM config

--- release/run.bat
+++ release/run.bat
@@ -1,1 +1,1 @@
-java -cp java-desktop-adapter-3.0.1.0.jar -DOpenFinOption="https://demoappdirectory.openf.in/desktop/config/apps/OpenFin/HelloOpenFin/app.json" com.openfin.desktop.demo.OpenFinDesktopDemo
+java -cp java-desktop-adapter-3.0.1.0.jar -DOpenFinOption=--config=\"https://demoappdirectory.openf.in/desktop/config/apps/OpenFin/HelloOpenFin/app.json\" com.openfin.desktop.demo.OpenFinDesktopDemo
