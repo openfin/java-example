515130acd48d15bfe958c18d56554043f4c9d64e
Wenjun Che <wenjun@openfin.co>
2019-03-10 13:09:08 -0400

    ADAP-142: Added examples of docking by Layout service
    * ADAP-142: Added examples of docking by Layout service

--- release/docking.bat
+++ release/docking.bat
@@ -1,6 +1,5 @@
-if "%1" == "" (
-set MainClass="com.openfin.desktop.demo.OpenFinDockingDemo"
-) else (
-set MainClass="%1"
-)
-java -cp openfin-desktop-java-example-6.0.0.3.jar;openfin-desktop-java-adapter-6.0.0.3-SNAPSHOT.jar;openfin-snap-dock-1.0.0.1.jar;TableLayout-20050920.jar;jna-4.1.0.jar;jna-platform-4.1.0.jar;json-20140107.jar;slf4j-api-1.7.5.jar;slf4j-jdk14-1.6.1.jar;websocket-api-9.3.12.v20160915.jar;websocket-client-9.3.12.v20160915.jar;websocket-common-9.3.12.v20160915.jar;jetty-io-9.3.12.v20160915.jar;jetty-util-9.3.12.v20160915.jar -Djava.util.logging.config.file=logging.properties  -Dcom.openfin.demo.version=5.44.12.27 -Dcom.openfin.temp=%LocalAppData%\OpenFin\temp %MainClass%
+REM batch to run Docking example of OpenFin and Java windows with Layout service.
+REM layout.html is a simple example of OpenFin window that uses Layout service.
+REM For this example to work, layout.html needs to be hosted by a web server and its URL needs to be configured with -Dcom.openfin.demo.layout.url
+
+java -cp openfin-desktop-java-example-7.1.1.jar;openfin-desktop-java-adapter-7.1.1-SNAPSHOT.jar;openfin-snap-dock-1.0.0.1.jar;TableLayout-20050920.jar;jna-4.1.0.jar;jna-platform-4.1.0.jar;json-20140107.jar;slf4j-api-1.7.5.jar;slf4j-jdk14-1.6.1.jar;websocket-api-9.3.12.v20160915.jar;websocket-client-9.3.12.v20160915.jar;websocket-common-9.3.12.v20160915.jar;jetty-io-9.3.12.v20160915.jar;jetty-util-9.3.12.v20160915.jar -Djava.util.logging.config.file=logging.properties -Dcom.openfin.demo.layout.url=http://localhost:8000/layout.html com.openfin.desktop.demo.LayoutServiceDemo
---
+++ src/main/java/com/openfin/desktop/demo/LayoutFrame.java
@@ -0,0 +1,124 @@
+package com.openfin.desktop.demo;
+
+/**
+ * Example of Java window that can be managed by OpenFin layout service for snap&dock
+ */
+
+import com.openfin.desktop.*;
+import com.openfin.desktop.Window;
+import com.openfin.desktop.channel.ChannelAction;
+import com.openfin.desktop.channel.ChannelClient;
+import com.openfin.desktop.win32.ExternalWindowObserver;
+import org.json.JSONObject;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.lang.System;
+
+public class LayoutFrame extends JFrame {
+    private ExternalWindowObserver externalWindowObserver;
+    private JButton btnUndock;
+    private String windowName;
+
+    public LayoutFrame(DesktopConnection desktopConnection, String appUuid, String windowName) throws DesktopException {
+        super();
+        System.out.println(windowName + " being created ");
+        this.windowName = windowName;
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.setPreferredSize(new Dimension(640, 480));
+        JPanel pnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        this.btnUndock = new JButton("undock");
+        this.btnUndock.setEnabled(false);
+        pnl.add(btnUndock);
+        this.getContentPane().add(pnl);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+        this.externalWindowObserver = new ExternalWindowObserver(desktopConnection.getPort(), appUuid, windowName,
+                        this, new AckListener() {
+            @Override
+            public void onSuccess(Ack ack) {
+                ExternalWindowObserver observer = (ExternalWindowObserver) ack.getSource();
+                observer.getDesktopConnection().getChannel().connect("of-layouts-service-v1",
+                        new AsyncCallback<ChannelClient>() {
+                            @Override
+                            public void onSuccess(ChannelClient client) {
+                                btnUndock.addActionListener(new ActionListener() {
+                                    @Override
+                                    public void actionPerformed(ActionEvent e) {
+                                        JSONObject payload = new JSONObject();
+                                        payload.put("uuid", appUuid);
+                                        payload.put("name", windowName);
+                                        client.dispatch("UNDOCK-WINDOW", payload, null);
+                                    }
+                                });
+
+                                client.register("event", new ChannelAction() {
+                                    @Override
+                                    public JSONObject invoke(String action, JSONObject payload) {
+                                        System.out.printf("channel event " + action);
+                                        return null;
+                                    }
+                                });
+                            }
+                        });
+            }
+            @Override
+            public void onError(Ack ack) {
+                System.out.println(windowName + ": unable to register external window, " + ack.getReason());
+            }
+        });
+
+        Window w = Window.wrap(appUuid, windowName, desktopConnection);
+        w.addEventListener("group-changed", new EventListener() {
+            @Override
+            public void eventReceived(com.openfin.desktop.ActionEvent actionEvent) {
+                JSONObject eventObj = actionEvent.getEventObject();
+                w.getGroup(new AsyncCallback<java.util.List<Window>>() {
+                    @Override
+                    public void onSuccess(java.util.List<Window> result) {
+                        if (result.size() > 0) {
+                            LayoutFrame.this.btnUndock.setEnabled(true);
+                        } else {
+                            LayoutFrame.this.btnUndock.setEnabled(false);
+                        }
+                    }
+                }, null);
+            }
+        }, null);
+
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                try {
+                    LayoutFrame.this.cleanup();
+                    LayoutFrame.this.dispose();
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+            public void windowClosed(WindowEvent e) {
+                super.windowClosed(e);
+                System.out.println(windowName + " closed ");
+            }
+        });
+    }
+
+    public String getWindowName() {
+        return windowName;
+    }
+
+    public void cleanup() {
+        try {
+            System.out.println(windowName + " cleaning up ");
+            this.externalWindowObserver.dispose();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
