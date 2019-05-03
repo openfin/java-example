0cf2b0df2accd07d9aa23f0114d53e94e2417c51
Wenjun Che <wenjun@openfin.co>
2019-05-03 17:03:25 -0400

    ADAP-142: rebase maseter

--- src/main/java/com/openfin/desktop/demo/LayoutFrame.java
+++ src/main/java/com/openfin/desktop/demo/LayoutFrame.java
@@ -9,12 +9,17 @@ import com.openfin.desktop.Window;
 import com.openfin.desktop.channel.ChannelAction;
 import com.openfin.desktop.channel.ChannelClient;
 import com.openfin.desktop.win32.ExternalWindowObserver;
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.WinDef;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.lang.System;
@@ -23,11 +28,20 @@ public class LayoutFrame extends JFrame {
     private ExternalWindowObserver externalWindowObserver;
     private JButton btnUndock;
     private String windowName;
+    private ChannelClient channelClient;
+    private String appUuid;
+    private boolean frameless;
 
     public LayoutFrame(DesktopConnection desktopConnection, String appUuid, String windowName) throws DesktopException {
+        this(desktopConnection, appUuid, windowName, false);
+    }
+
+    public LayoutFrame(DesktopConnection desktopConnection, String appUuid, String windowName, boolean frameless) throws DesktopException {
         super();
         System.out.println(windowName + " being created ");
+        this.appUuid = appUuid;
         this.windowName = windowName;
+        this.frameless = frameless;
         this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         this.setPreferredSize(new Dimension(640, 480));
         JPanel pnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
@@ -35,40 +49,93 @@ public class LayoutFrame extends JFrame {
         this.btnUndock.setEnabled(false);
         pnl.add(btnUndock);
         this.getContentPane().add(pnl);
+
+        if (frameless) {
+            this.setUndecorated(true);
+            JPanel titleBar = new JPanel(new BorderLayout());
+            titleBar.setBackground(Color.DARK_GRAY);
+            MouseAdapter myListener = new MouseAdapter() {
+                int pressedAtX, pressedAtY;
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    pressedAtX = e.getX();
+                    pressedAtY = e.getY();
+                    System.out.println("mouse pressed at x=" + pressedAtX + ", y=" + pressedAtY);
+                }
+
+                @Override
+                public void mouseDragged(MouseEvent e) {
+                    int distanceX = e.getX() - pressedAtX;
+                    int distanceY = e.getY() - pressedAtY;
+                    System.out.println("dragged x=" + distanceX + ", y=" + distanceY);
+                    Point frameLocation = LayoutFrame.this.getLocation();
+                    LayoutFrame.this.setLocation(frameLocation.x + distanceX, frameLocation.y + distanceY);
+                }
+            };
+            titleBar.addMouseListener(myListener);
+            titleBar.addMouseMotionListener(myListener);
+
+            JButton btnClose = new JButton("X");
+            btnClose.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    LayoutFrame.this.dispose();
+                }});
+            titleBar.add(btnClose, BorderLayout.EAST);
+
+            this.getContentPane().add(titleBar, BorderLayout.NORTH);
+
+        }
+
         this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
 
-        this.externalWindowObserver = new ExternalWindowObserver(desktopConnection.getPort(), appUuid, windowName,
-                        this, new AckListener() {
-            @Override
-            public void onSuccess(Ack ack) {
-                ExternalWindowObserver observer = (ExternalWindowObserver) ack.getSource();
-                observer.getDesktopConnection().getChannel().connect("of-layouts-service-v1").thenAccept(client->{
-                    btnUndock.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            JSONObject payload = new JSONObject();
-                            payload.put("uuid", appUuid);
-                            payload.put("name", windowName);
-                            client.dispatch("UNDOCK-WINDOW", payload);
-                        }
-                    });
+        this.externalWindowObserver = new ExternalWindowObserver(desktopConnection.getPort(), appUuid, windowName, this,
+                new AckListener() {
+                    @Override
+                    public void onSuccess(Ack ack) {
+                        ExternalWindowObserver observer = (ExternalWindowObserver) ack.getSource();
+                        observer.getDesktopConnection().getChannel().connect("of-layouts-service-v1").thenAccept(client -> {
+                            LayoutFrame.this.channelClient = client;
+                            btnUndock.addActionListener(new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent e) {
+                                    JSONObject payload = new JSONObject();
+                                    payload.put("uuid", appUuid);
+                                    payload.put("name", windowName);
+                                    client.dispatch("UNDOCK-WINDOW", payload);
+                                }
+                            });
 
-                    client.register("event", new ChannelAction() {
-                        @Override
-                        public JSONObject invoke(String action, JSONObject payload) {
-                            System.out.printf("channel event " + action);
-                            return null;
-                        }
-                    });
+                            client.register("event", new ChannelAction() {
+                                @Override
+                                public JSONObject invoke(String action, JSONObject payload) {
+                                    System.out.printf("channel event " + action);
+                                    return null;
+                                }
+                            });
+                        });
+
+                    }
+
+                    @Override
+                    public void onError(Ack ack) {
+                        System.out.println(windowName + ": unable to register external window, " + ack.getReason());
+                    }
                 });
+        this.externalWindowObserver.setUserGesture(!this.frameless);
+        try {
+            if (this.frameless) {
+                WindowOptions options = new WindowOptions();
+                options.setFrame(false);
+                this.externalWindowObserver.setWindowOptions(options);
             }
-            @Override
-            public void onError(Ack ack) {
-                System.out.println(windowName + ": unable to register external window, " + ack.getReason());
-            }
-        });
+            this.externalWindowObserver.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         Window w = Window.wrap(appUuid, windowName, desktopConnection);
         w.addEventListener("group-changed", new EventListener() {
@@ -79,9 +146,10 @@ public class LayoutFrame extends JFrame {
                     @Override
                     public void onSuccess(java.util.List<Window> result) {
                         if (result.size() > 0) {
-                            LayoutFrame.this.btnUndock.setEnabled(true);
+                            checkTabbing();
                         } else {
                             LayoutFrame.this.btnUndock.setEnabled(false);
+                            setHasFrame(LayoutFrame.this, true);
                         }
                     }
                 }, null);
@@ -94,15 +162,60 @@ public class LayoutFrame extends JFrame {
                 try {
                     LayoutFrame.this.cleanup();
                     LayoutFrame.this.dispose();
-                } catch (Exception e1) {
+                }
+                catch (Exception e1) {
                     e1.printStackTrace();
                 }
             }
+
             public void windowClosed(WindowEvent e) {
                 super.windowClosed(e);
                 System.out.println(windowName + " closed ");
             }
         });
+    }
+
+    private void checkTabbing() {
+        JSONObject payload = new JSONObject();
+        payload.put("uuid", appUuid);
+        payload.put("name", windowName);
+        channelClient.dispatch("GETTABS", payload).thenAccept(ack -> {
+            if (ack.isSuccessful()) {
+                System.out.printf("channel GETTABS ");
+                JSONObject data = (JSONObject) ack.getData();
+                Object result = data.get("result");
+                if (result != null && result instanceof JSONArray) {
+                    JSONArray tabs = (JSONArray) result;
+                    boolean enabled = !(tabs != null && tabs.length() > 0);
+                    LayoutFrame.this.btnUndock.setEnabled(enabled);
+                    setHasFrame(LayoutFrame.this, false);
+                } else {
+                    LayoutFrame.this.btnUndock.setEnabled(true);
+                    setHasFrame(LayoutFrame.this, true);
+                }
+            } else {
+                System.out.printf("channel GETTABS error " + ack.getReason());
+            }
+        });
+    }
+
+    private void setHasFrame(JFrame frame, boolean hasFrame) {
+        if (!this.frameless) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(windowName + " hasFrame=" + hasFrame);
+                    WinDef.HWND hWnd = new WinDef.HWND();
+                    hWnd.setPointer(Native.getComponentPointer(frame));
+                    LayoutFrame.this.externalWindowObserver.setHasFrame(hWnd, hasFrame);
+                    frame.setResizable(hasFrame);
+                    frame.invalidate();
+                    frame.validate();
+                    frame.repaint();
+                    SwingUtilities.updateComponentTreeUI(frame);
+                }
+            });
+        }
     }
 
     public String getWindowName() {
@@ -113,8 +226,53 @@ public class LayoutFrame extends JFrame {
         try {
             System.out.println(windowName + " cleaning up ");
             this.externalWindowObserver.dispose();
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+
+    public static void main(String[] args) {
+        final JFrame frame = new JFrame();
+        frame.setPreferredSize(new Dimension(640, 480));
+        JPanel pnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        frame.setUndecorated(true);
+        JPanel titleBar = new JPanel(new BorderLayout());
+        titleBar.setBackground(Color.DARK_GRAY);
+        MouseAdapter myListener = new MouseAdapter() {
+            int pressedAtX, pressedAtY;
+            @Override
+            public void mousePressed(MouseEvent e) {
+                pressedAtX = e.getX();
+                pressedAtY = e.getY();
+                System.out.println("mouse pressed at x=" + pressedAtX + ", y=" + pressedAtY);
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                int distanceX = e.getX() - pressedAtX;
+                int distanceY = e.getY() - pressedAtY;
+                System.out.println("dragged x=" + distanceX + ", y=" + distanceY);
+                Point frameLocation = frame.getLocation();
+                frame.setLocation(frameLocation.x + distanceX, frameLocation.y + distanceY);
+            }
+        };
+        titleBar.addMouseListener(myListener);
+        titleBar.addMouseMotionListener(myListener);
+
+        JButton btnClose = new JButton("X");
+        btnClose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }});
+        titleBar.add(btnClose, BorderLayout.EAST);
+        frame.getContentPane().add(titleBar, BorderLayout.NORTH);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 }
