a4f41dc20c9a23647a31735e0dd5a67c42262835
wenjunche <wenjun@openfin.co>
2021-11-20 11:25:19 -0500

    ADAP-205: updated some dependencies to match adapter

--- release/channel.bat
+++ release/channel.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 REM This program is an example of channel API.   It can be run as channel provider or client
+REM more information can be found in comments of com.openfin.desktop.demo.ChannelExample.java
 REM Channel name: ChannelExample
 REM usage:  channel.bat [ provider | client ]
 set mode=%1
 IF "%~1" == "" set mode=provider
 echo Channel type set to: %mode%
-java -cp lib/hamcrest-core-1.3.jar;lib/hamcrest-library-1.1.jar;lib/jetty-client-9.4.18.v20190429.jar;lib/jetty-http-9.4.18.v20190429.jar;lib/jetty-io-9.4.18.v20190429.jar;lib/jetty-util-9.4.18.v20190429.jar;lib/jetty-xml-9.4.18.v20190429.jar;lib/jna-4.5.1.jar;lib/jna-platform-4.5.1.jar;lib/json-20160810.jar;lib/junit-4.11.jar;lib/mockito-core-1.9.5.jar;lib/objenesis-1.0.jar;lib/openfin-desktop-java-adapter-9.0.1-SNAPSHOT.jar;lib/openfin-desktop-java-example-9.0.1.jar;lib/slf4j-api-1.7.21.jar;lib/slf4j-jdk14-1.6.1.jar;lib/slf4j-log4j12-1.7.18.jar;lib/TableLayout-20050920.jar;lib/websocket-api-9.4.18.v20190429.jar;lib/websocket-client-9.4.18.v20190429.jar;lib/websocket-common-9.4.18.v20190429.jar -Djava.util.logging.config.file=logging.properties -Dcom.openfin.demo.version=stable com.openfin.desktop.demo.ChannelExample %mode%
+java -cp lib/hamcrest-core-1.3.jar;lib/hamcrest-library-1.1.jar;lib/jetty-client-9.4.18.v20190429.jar;lib/jetty-http-9.4.18.v20190429.jar;lib/jetty-io-9.4.18.v20190429.jar;lib/jetty-util-9.4.18.v20190429.jar;lib/jetty-xml-9.4.18.v20190429.jar;lib/jna-5.9.0.jar;lib/jna-platform-5.9.0.jar;lib/json-20210307.jar;lib/junit-4.11.jar;lib/mockito-core-1.9.5.jar;lib/objenesis-1.0.jar;lib/openfin-desktop-java-adapter-10.0.1-SNAPSHOT.jar;lib/openfin-desktop-java-example-10.0.1.jar;lib/slf4j-api-1.7.21.jar;lib/slf4j-jdk14-1.6.1.jar;lib/slf4j-log4j12-1.7.18.jar;lib/TableLayout-20050920.jar;lib/websocket-api-9.4.18.v20190429.jar;lib/websocket-client-9.4.18.v20190429.jar;lib/websocket-common-9.4.18.v20190429.jar;lib/webrtc-java-0.3.0.jar;lib/webrtc-java-0.3.0-windows-x86_64.jar -Djava.util.logging.config.file=logging.properties -Dcom.openfin.demo.runtime.version=24.96.67.4 com.openfin.desktop.demo.ChannelExample %mode%
 
--- src/main/java/com/openfin/desktop/demo/ChannelExample.java
+++ src/main/java/com/openfin/desktop/demo/ChannelExample.java
@@ -1,3 +1,52 @@
+/**
+ * Example for Channel API in Java adapter
+ *
+ * This example can run as a channel Provider or channel Client.  In both cases,  it needs a OpenFin app to demonstrate how channel APIs can be used for communication
+ *
+ * As a channel provider:
+ *
+ *  1. start this example with 'provider' as an argument
+ *  2. start an OpenFin app and run the following javascript, which creates channel client and sends actions to this example to process
+ *
+ *	    const client = await fin.InterApplicationBus.Channel.connect('ChannelExample', { protocols: ['classic'] });  // replace 'classic' with 'rtc' for webrtc based channels
+ * 	    const value = await client.dispatch('getValue');  // 'getValue' is processed by a channel provider created by Java example
+ * 	    console.log(value);
+ *
+ * As a channel client:
+ *
+ *  1. start an OpenFin app and run the following javascript, which creates channel provider and waits for connection from Java example
+ *
+ *      const providerBus = await fin.InterApplicationBus.Channel.create('ChannelExample', { protocols: ['rtc', 'classic'] });
+ *      provider.onConnection((identity, payload) => {
+ *              console.log('Client connection request identity: ', JSON.stringify(identity));
+ *              console.log('Client connection request payload: ', JSON.stringify(payload));
+ *              });
+ *      provider.onDisconnection(evt => {
+ *              console.log('Client disconnected', `uuid: ${evt.uuid}, name: ${evt.name}`);
+ *      });
+ *
+ *      let value = 123;
+ *      provider.register('getValue', (payload, identity) => {
+ *          console.log('Action dispatched by client: ', JSON.stringify(identity));
+ *          console.log('Payload sent in dispatch: ', JSON.stringify(payload));
+ *          return {
+ *              value: value
+ *          };
+ *      });
+ *      providerBus.register('increment', (payload, identity) => {
+ *          console.log('Action dispatched by client: ', JSON.stringify(identity));
+ *          console.log('Payload sent in dispatch: ', JSON.stringify(payload));
+ * 	        value = value + payload.value;
+ *          return {
+ *            value: value
+ *          };
+ *      });
+ *
+ *  2. start this example with 'client' as an argument
+ *
+ *  Channel API support Channel.CLASSIC_PROTOCOL and  Channel.RTC_PROTOCOL.  Version 24.96.67.4+ of Runtime is required for Channel.RTC_PROTOCOL.
+ *
+ */
 package com.openfin.desktop.demo;
 
 import java.util.Arrays;
@@ -9,11 +58,7 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.openfin.desktop.Ack;
-import com.openfin.desktop.AckListener;
-import com.openfin.desktop.AsyncCallback;
 import com.openfin.desktop.DesktopConnection;
-import com.openfin.desktop.DesktopException;
 import com.openfin.desktop.DesktopStateListener;
 import com.openfin.desktop.RuntimeConfiguration;
 
@@ -108,7 +153,7 @@ public class ChannelExample implements DesktopStateListener {
                     logger.info("current value={}", ack.getJsonObject().getJSONObject("data").getJSONObject("result").getInt("value"));
                     //let's do increment by 10
                     JSONObject payload = new JSONObject();
-                    payload.put("delta", 10);
+                    payload.put("value", 10);
                     client.dispatchAsync("increment", payload).thenAccept(incAck -> {
                         if (incAck.isSuccessful()) {
                             logger.info("after invoking incrementBy, value={}", incAck.getJsonObject().getJSONObject("data").getJSONObject("result").getInt("value"));
